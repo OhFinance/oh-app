59d8b0151d5c1b7e5df36474cc16824f1d8fb2df
liquidsoft <info@liquid.software>
2021-12-05 02:36:49 -0800

    App Drawer Formatting, Add Text Skeleton

--- src/components/AppDrawer/components/AppDrawerPrices.tsx
+++ src/components/AppDrawer/components/AppDrawerPrices.tsx
@@ -1,6 +1,6 @@
-import { Grid } from "@material-ui/core";
-import { Skeleton } from "@material-ui/lab";
+import { Box, Grid } from "@material-ui/core";
 import { Flex, Text } from "@ohfinance/oh-ui";
+import { TextSkeleton } from "components/Skeleton";
 import { supportedTokenPrices } from "config/constants/tokens";
 import { usePriceManager } from "state/prices/hooks";
 
@@ -8,19 +8,21 @@ export const AppDrawerPrices = () => {
   const [prices] = usePriceManager();
 
   return (
-    <Grid container spacing={1} justify="center">
+    <Grid container justify="center">
       {supportedTokenPrices.map((symbol, i) => (
         <Grid item key={i} xs={8}>
-          {prices[symbol] !== undefined ? (
-            <Flex align="center" justify="space-between">
-              <Text style={{ fontSize: 11 }}>{symbol}</Text>
-              <Text style={{ fontSize: 11 }}>
-                <b>{prices[symbol].toPrecision(5)} USD</b>
-              </Text>
-            </Flex>
-          ) : (
-            <Skeleton width={160} height={20} />
-          )}
+          <Box mb={1}>
+            {prices[symbol] !== undefined ? (
+              <Flex align="center" justify="space-between">
+                <Text style={{ fontSize: 11 }}>{symbol}</Text>
+                <Text style={{ fontSize: 11 }}>
+                  <b>{prices[symbol]?.toPrecision(5)} USD</b>
+                </Text>
+              </Flex>
+            ) : (
+              <TextSkeleton width={160} height={20} />
+            )}
+          </Box>
         </Grid>
       ))}
     </Grid>
